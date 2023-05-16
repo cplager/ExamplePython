827a2383aea5a96906a99da2063989abb573c871
Charles Plager <cplager_github@email.plager.net>
2023-05-16 11:34:26 -0400

    added tree printout

--- AVL_tree.py
+++ AVL_tree.py
@@ -4,11 +4,90 @@
 class TreeNode(object):
     """Note of tree"""
     def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
+        self.value  = val
+        self.left   = None
+        self.right  = None
         self.height = 1
- 
+
+    def stringify(self, maxWidth=2):
+        grid = TreeNode.add_node_to_grid(self)
+        return TreeNode.stringify_grid(grid, maxWidth)
+    
+
+    def __str__(self):
+        return self.stringify()
+    
+
+    @staticmethod
+    def add_node_to_grid(node, grid=None, yPos=0, xPos=0):
+        """Used to print out tree at end"""
+        if grid is None:
+            grid = {}
+        grid.setdefault(yPos, {})
+        grid[yPos][xPos] = node
+        if node.left is not None:
+            # are we positive or negative?
+            newXpos = -1 # only true in root case
+            if xPos < 0:
+                newXpos = 2 * xPos
+            elif xPos > 0:
+                newXpos = 2 * xPos - 1
+            TreeNode.add_node_to_grid(node.left, grid, yPos + 1, newXpos)
+        if node.right is not None:
+            # are we positive or negative?
+            newXpos = 1 # only true in root case
+            if xPos < 0:
+                newXpos = 2 * xPos + 1
+            elif xPos > 0:
+                newXpos = 2 * xPos
+            TreeNode.add_node_to_grid(node.right, grid, yPos + 1, newXpos)
+        return grid
+    
+
+    @staticmethod
+    def simple_stringify_grid(grid):
+        """Diagnostic stringification to make sure grid setup correctly."""
+        retStr = ''
+        for layer, row in sorted(grid.items()):
+            retStr += f'{layer} :  '
+            for xPos, node in sorted(row.items()):
+                nodeVal = f'{node.value:2}'
+                extra   = ''
+                if node.left:
+                    extra += f'L{node.left.value:2}'
+                if node.right:
+                    if extra:
+                        extra += ' '
+                    extra += f'R{node.right.value:2}'
+                if extra:
+                    nodeVal += f' ({extra})'
+                retStr += f'{xPos:3} {nodeVal},  '
+            retStr += '\n'
+        return retStr
+
+
+    @staticmethod
+    def stringify_grid(grid, maxWidth=2):
+        # how many layers deep is this grid
+        deep   = len(grid)
+        fmtStr = '{:<%d}' % maxWidth
+        retStr = ''
+        for layer, row in sorted(grid.items()):
+            # TODO: put in /\ where appropriate
+            spacing = int(2 ** (deep - layer) + 0.01)
+            maxPos  = int(2 ** (layer - 1) + 0.01)
+            if not maxPos:
+                spacing += 2
+            retStr += ' ' * spacing
+            for pos in range(-maxPos, maxPos + 1):
+                node = row.get(pos)
+                if node:
+                    value = node.value
+                else:
+                    value = ''
+                retStr += fmtStr.format(value) + ' ' * spacing
+            retStr += '\n'
+        return retStr
 
 
 # AVL tree class which supports the
@@ -23,7 +102,7 @@ class AVL_Tree(object):
         # Step 1 - Perform normal BST
         if not root:
             return TreeNode(key)
-        elif key < root.val:
+        elif key < root.value:
             root.left = self.insert(root.left, key)
         else:
             root.right = self.insert(root.right, key)
@@ -39,20 +118,20 @@ class AVL_Tree(object):
         # Step 4 - If the node is unbalanced,
         # then try out the 4 cases
         # Case 1 - Left Left
-        if balance > 1 and key < root.left.val:
+        if balance > 1 and key < root.left.value:
             return self.rightRotate(root)
  
         # Case 2 - Right Right
-        if balance < -1 and key > root.right.val:
+        if balance < -1 and key > root.right.value:
             return self.leftRotate(root)
  
         # Case 3 - Left Right
-        if balance > 1 and key > root.left.val:
+        if balance > 1 and key > root.left.value:
             root.left = self.leftRotate(root.left)
             return self.rightRotate(root)
  
         # Case 4 - Right Left
-        if balance < -1 and key < root.right.val:
+        if balance < -1 and key < root.right.value:
             root.right = self.rightRotate(root.right)
             return self.leftRotate(root)
  
@@ -116,7 +195,7 @@ class AVL_Tree(object):
         if not root:
             return
  
-        print(f"{root.val} ", end="")
+        print(f"{root.value} ", end="")
         self.preOrder(root.left)
         self.preOrder(root.right)
  
@@ -133,15 +212,17 @@ if __name__ == '__main__':
     root = myTree.insert(root, 50)
     root = myTree.insert(root, 25)
     
-    """The constructed AVL Tree would be
+    """
+    The constructed AVL Tree would be
              30
             /  \
            20   40
-          /  \     \
-         10  25    50"""
+          /  \    \
+         10  25    50
+    """
     
     # Preorder Traversal
     print("Preorder traversal of the",
         "constructed AVL tree is")
     myTree.preOrder(root)
-    print()
+    print(f'\n{root}\n')
